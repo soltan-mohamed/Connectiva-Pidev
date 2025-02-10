830dee101fb74ada099ea843fa060c713b6bd18f
adamwahada <adam.wahada@esprit.tn>
2025-02-10 15:38:59 +0100

    testadam

--- Backend/AIAlgorithme/src/main/java/tn/esprit/aialgorithme/model/User.java
+++ Backend/AIAlgorithme/src/main/java/tn/esprit/aialgorithme/model/User.java
@@ -23,7 +23,7 @@ public class User {
 
     @Column(unique = true, nullable = false)
     private String email;
-
+//test
     @ElementCollection
     private List<String> interests = new ArrayList<>();
 
